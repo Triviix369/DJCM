a4a69437c11acbad7d9364cb64b3c2672ca0a9b5
Chris-JW <chriswai0513@gmail.com>
2023-03-11 12:33:57 +0800

    AuthorSubmission In progress

--- src/pages/dashboard/authorSubmission/AuthorHome.js
+++ src/pages/dashboard/authorSubmission/AuthorHome.js
@@ -32,7 +32,7 @@ import {
   TablePaginationCustom,
 } from '../../../components/table';
 // sections
-import { UserTableToolbar, UserTableRow } from '../../../sections/@dashboard/user/list';
+// import { AuthorJournalTableToolbar, UserTableRow } from '../../../sections/@dashboard/authorSubmission/list';
 
 // ----------------------------------------------------------------------
 export default function AuthorHome() {
@@ -49,7 +49,7 @@ export default function AuthorHome() {
                     heading="Author Submission Listing"
                     links={[
                         { name: 'Dashboard', href: PATH_DASHBOARD.root },
-                        { name: 'List' },
+                        { name: 'Listing' },
                     ]}
                     action={
                         <Button
@@ -58,109 +58,13 @@ export default function AuthorHome() {
                             variant="contained"
                             startIcon={<Iconify icon="eva:plus-fill" />}
                         >
-                            New User
+                            New Submission
                         </Button>
                     }
                 />
-                <Card>
-                    {/* <Tabs
-                        value={filterStatus}
-                        onChange={handleFilterStatus}
-                        sx={{
-                            px: 2,
-                            bgcolor: 'background.neutral',
-                        }}
-                    >
-                        {STATUS_OPTIONS.map((tab) => (
-                            <Tab key={tab} label={tab} value={tab} />
-                        ))}
-                    </Tabs> */}
-
-                    <Divider />
-
-                    {/* <UserTableToolbar
-                        isFiltered={isFiltered}
-                        filterName={filterName}
-                        filterRole={filterRole}
-                        optionsRole={ROLE_OPTIONS}
-                        onFilterName={handleFilterName}
-                        onFilterRole={handleFilterRole}
-                        onResetFilter={handleResetFilter}
-                    />
-
-                    <TableContainer sx={{ position: 'relative', overflow: 'unset' }}>
-                        <TableSelectedAction
-                            dense={dense}
-                            numSelected={selected.length}
-                            rowCount={tableData.length}
-                            onSelectAllRows={(checked) =>
-                                onSelectAllRows(
-                                    checked,
-                                    tableData.map((row) => row.StaffID)
-                                )
-                            }
-                            action={
-                                <Tooltip title="Delete">
-                                    <IconButton color="primary" onClick={handleOpenConfirm}>
-                                        <Iconify icon="eva:trash-2-outline" />
-                                    </IconButton>
-                                </Tooltip>
-                            }
-                        />
-
-                        <Scrollbar>
-                            <Table size={dense ? 'small' : 'medium'} sx={{ minWidth: 800 }}>
-                                <TableHeadCustom
-                                    order={order}
-                                    orderBy={orderBy}
-                                    headLabel={TABLE_HEAD}
-                                    rowCount={tableData.length}
-                                    numSelected={selected.length}
-                                    onSort={onSort}
-                                    onSelectAllRows={(checked) =>
-                                        onSelectAllRows(
-                                            checked,
-                                            tableData.map((row) => row.StaffID)
-                                        )
-                                    }
-                                />
-
-                                <TableBody>
-                                    {dataFiltered
-                                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                        .map((row) => (
-                                            <UserTableRow
-                                                key={row.StaffID}
-                                                row={row}
-                                                selected={selected.includes(row.StaffID)}
-                                                onSelectRow={() => onSelectRow(row.StaffID)}
-                                                onDeleteRow={() => handleDeleteRow(row.StaffID)}
-                                                onEditRow={() => handleEditRow(row.StaffID)}
-                                            />
-                                        ))}
-
-                                    <TableEmptyRows
-                                        height={denseHeight}
-                                        emptyRows={emptyRows(page, rowsPerPage, tableData.length)}
-                                    />
-
-                                    <TableNoData isNotFound={isNotFound} />
-                                </TableBody>
-                            </Table>
-                        </Scrollbar>
-                    </TableContainer>
-
-                    <TablePaginationCustom
-                        count={dataFiltered.length}
-                        page={page}
-                        rowsPerPage={rowsPerPage}
-                        onPageChange={onChangePage}
-                        onRowsPerPageChange={onChangeRowsPerPage}
-                        //
-                        dense={dense}
-                        onChangeDense={onChangeDense}
-                    /> */}
-                </Card>
+                {/* <Card>
+                    
+                </Card> */}
             </Container>
         </>
     );
